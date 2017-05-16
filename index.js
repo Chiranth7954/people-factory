e004b49c3cdf3406b6550cedb9c39b0fdce07931
Chiranth7954 <ckishore@purdue.edu>
2017-05-16 14:31:10 -0400

    Avoid hard-coding field names from class

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ const renderItem = (name, value) => {
 
 const renderList = (person) => {
   const listRendered = document.createElement('ul')
-  Object.keys(person).map((key, _i, _keys) => {
-    const li = renderItem(key, person[key])
-    listRendered.appendChild(li)
+  Array.from(person).map((input, _i, _formElements) => {
+    if(input.value) {
+      const li = renderItem(input.name, input.value)
+      listRendered.appendChild(li)
+    }
   })
   return listRendered
 }
@@ -28,14 +30,7 @@ const handleSubmit = (ev) => {
   const form = ev.target
   const details = document.querySelector('.details')
 
-  const person = {
-    personName: form.personName.value,
-    hairColor: renderColor(form.hairColor.value).outerHTML,
-    age: form.age.value,
-    birthplace: form.birthplace.value,
-  }
-
-  const list = renderList(person)
+  const list = renderList(form.elements)
   details.appendChild(list)
 
   /*const elements = document.getElementsByTagName("input")
